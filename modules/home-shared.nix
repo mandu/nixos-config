5b81e302640a29ea3964cde1906f967bafccc67a
Mikko Haavisto <mvi.haavisto@gmail.com>
2022-11-06 11:46:07 +0200

    update nvim-lspconfig

--- modules/home-shared.nix
+++ modules/home-shared.nix
@@ -335,12 +335,20 @@ in
         plugin = nvim-lspconfig;
         config = ''
           lua << EOF
+          -- Mappings.
+          -- See `:help vim.diagnostic.*` for documentation on any of the below functions
+          local opts = { noremap=true, silent=true }
+          vim.keymap.set('n', '<space>e', vim.diagnostic.open_float, opts)
+          vim.keymap.set('n', '[d', vim.diagnostic.goto_prev, opts)
+          vim.keymap.set('n', ']d', vim.diagnostic.goto_next, opts)
+          vim.keymap.set('n', '<space>q', vim.diagnostic.setloclist, opts)
+
           -- Use an on_attach function to only map the following keys
           -- after the language server attaches to the current buffer
           local on_attach = function(client, bufnr)
             -- Enable completion triggered by <c-x><c-o>
             vim.api.nvim_buf_set_option(bufnr, 'omnifunc', 'v:lua.vim.lsp.omnifunc')
-          
+
             -- Mappings.
             -- See `:help vim.lsp.*` for documentation on any of the below functions
             local bufopts = { noremap=true, silent=true, buffer=bufnr }
@@ -358,7 +366,7 @@ in
             vim.keymap.set('n', '<space>rn', vim.lsp.buf.rename, bufopts)
             vim.keymap.set('n', '<space>ca', vim.lsp.buf.code_action, bufopts)
             vim.keymap.set('n', 'gr', vim.lsp.buf.references, bufopts)
-            vim.keymap.set('n', '<space>f', vim.lsp.buf.formatting, bufopts)
+            vim.keymap.set('n', '<space>f', function() vim.lsp.buf.format { async = true } end, bufopts)
           end
           
           local lsp_flags = {
